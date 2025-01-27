1020f7f7d2d8d9cf703199a62b89ac1d1a439fc9
SirMaxeN <maxenkom@gmail.com>
2025-01-27 23:42:18 +0100

    add colors to scripts

--- help_cli_advance.sh
+++ help_cli_advance.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-BLUE="\033[1;34m"
-CYAN="\033[1;36m"
+YELLOW="\033[1;33m"
+GREY="\033[1;30m"
+RED="\033[1;31m"
 GREEN="\033[1;32m"
-NC="\033[0m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+NO_COLOR="\033[0m"
 
 echo -e "\ADVANCE COMMAND IN CLI\n"
-echo -e "${BLUE}xxd${CYAN}file ${NC}- display bytes file in hexadecimal format"
+echo -e "${BLUE}xxd${CYAN}file ${NO_COLOR}- display bytes file in hexadecimal format"
 echo ""
